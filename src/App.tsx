7a64dcad4a4987ffe56461381274ea000eba3e18
Gustavo Petruzzi <gustavo.petruzzi@softvision.com>
2022-12-04 16:28:19 -0300

    Add features for day 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,51 @@
-export const App = () => {
-  const gifts: string[] = [
-    'Medias',
-    'Caramelos',
-    'Vitel tone'
-  ]
-  return (
-    <>
-      <h1> Regalos </h1>
-      <ul>
-        {gifts.map(gift => (
-          <li key={gift}>{gift}</li>
-        ))}
+import { useRef, useState } from 'react';
+import './App.css';
+import CustomListItem from './components/custom-list-item/Custom-list-item';
 
-      </ul>
-    </>
+const INITIAL_GIFTS: string[] = [
+  'Medias',
+  'Caramelos',
+  'Vitel tone'
+];
+export const App = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [giftList, setGiftList] = useState<string[]>(INITIAL_GIFTS);
+
+  const addGift = () => {
+    const gift = inputRef.current?.value;
+    if (gift) {
+      setGiftList([...giftList, gift]);
+      inputRef.current.value = '';
+    }
+  };
+  const removeGift = (gift: string) => {
+    const newGiftList = giftList.filter((g) => g !== gift);
+    setGiftList(newGiftList);
+  }
+  return (
+    <section className="main">
+      <div className="gifts">
+        <h1> Regalos </h1>
+        <div className="input-container">
+          <input
+            ref={inputRef}
+            type="text"
+            placeholder="Agregar regalo"
+          />
+          <button onClick={addGift}> Agregar </button>
+        </div>
+        <ul>
+          {giftList.map(gift => (
+            <CustomListItem key={gift}>
+              <div className="list-item-container">
+                <span> {gift} </span>
+                <button className="remove-button" onClick={() => removeGift(gift)}> x </button>
+              </div>
+            </CustomListItem>
+          ))}
+
+        </ul>
+      </div>
+    </section>
   )
 }
